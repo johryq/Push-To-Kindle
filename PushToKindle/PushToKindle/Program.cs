6540bfe1cd9e1138559c5d5625fc551d8be50cd9
johryq <2686175746@qq.com>
2020-03-29 22:13:21 +0800

    first commit

---
+++ PushToKindle/PushToKindle/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.IO.Compression;
+using System.IO;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace PushToKindle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RequestOptions options = new RequestOptions();
+            options.Uri = new Uri("https://www.biquge5200.cc/130_130510/");
+            IWebProxy proxy = GetProxy();
+            string result = string.Empty;
+            //IWebProxy proxy =
+            var request = (HttpWebRequest)WebRequest.Create(options.Uri);
+            //POST大于1024字节时,POST请求分成两步
+            //发送一个请求询问Server是否接受 Exprct:100 -continue
+            //不是所有Server都会响应 100 -continue; 此时就会出现错误
+            request.ServicePoint.Expect100Continue = false;
+            request.ServicePoint.UseNagleAlgorithm = false;
+            if (!string.IsNullOrEmpty(options.XHRParams))
+            {
+                request.AllowWriteStreamBuffering = true;
+            }
+            else
+            {
+                request.AllowWriteStreamBuffering = false;
+            }
+            request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate, br");
+            request.ContentType = options.ContentType;
+            request.AllowAutoRedirect = options.AllowAutoRedirect;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36";
+            request.Timeout = options.Timeout;
+            request.KeepAlive = options.KeepAlive;
+            if (!string.IsNullOrEmpty(options.Referer)) request.Referer = options.Referer;//返回上一级历史链接
+            request.Method = options.Method="GET";
+            if (proxy != null) request.Proxy = proxy;//设置代理服务器IP，伪装请求地址
+            if (!string.IsNullOrEmpty(options.RequestCookies)) request.Headers[HttpRequestHeader.Cookie] = options.RequestCookies;
+            request.ServicePoint.ConnectionLimit = options.ConnectionLimit;//定义最大连接数
+            if (options.WebHeader != null && options.WebHeader.Count > 0) request.Headers.Add(options.WebHeader);//添加头部信息
+            if (!string.IsNullOrEmpty(options.XHRParams))//如果是POST请求，加入POST数据
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(options.XHRParams);
+                if (buffer != null)
+                {
+                    request.ContentLength = buffer.Length;
+                    request.GetRequestStream().Write(buffer, 0, buffer.Length);
+                }
+            }
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                ////获取请求响应
+                //foreach (Cookie cookie in response.Cookies)
+                //    options.CookiesContainer.Add(cookie);//将Cookie加入容器，保存登录状态
+                if (response.ContentEncoding.ToLower().Contains("gzip"))//解压
+                {
+                    using (GZipStream stream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress))
+                    {
+                        using (StreamReader reader = new StreamReader(stream,Encoding.GetEncoding("GBK")))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+                else if (response.ContentEncoding.ToLower().Contains("deflate"))//解压
+                {
+                    using (DeflateStream stream = new DeflateStream(response.GetResponseStream(), CompressionMode.Decompress))
+                    {
+                        using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+                else
+                {
+                    using (Stream stream = response.GetResponseStream())//原始
+                    {
+                        using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            request.Abort();
+            //Console.WriteLine(result);
+            GetNextUrl(result);
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// 代理访问
+        /// </summary>
+        /// <returns></returns>
+        private static System.Net.WebProxy GetProxy()
+        {
+            System.Net.WebProxy webProxy = null;
+            //try
+            //{
+            //    // 代理链接地址加端口
+            //    string proxyHost = "192.168.1.1";
+            //    string proxyPort = "9030";
+
+            //    // 代理身份验证的帐号跟密码
+            //    //string proxyUser = "xxx";
+            //    //string proxyPass = "xxx";
+
+            //    // 设置代理服务器
+            //    webProxy = new System.Net.WebProxy();
+            //    // 设置代理地址加端口
+            //    webProxy.Address = new Uri(string.Format("{0}:{1}", proxyHost, proxyPort));
+            //    // 如果只是设置代理IP加端口，例如192.168.1.1:80，这里直接注释该段代码，则不需要设置提交给代理服务器进行身份验证的帐号跟密码。
+            //    //webProxy.Credentials = new System.Net.NetworkCredential(proxyUser, proxyPass);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("获取代理信息异常", DateTime.Now.ToString(), ex.Message);
+            //}
+            return webProxy;
+        }
+
+
+        /// <summary>
+        /// 获取目录和章节URL
+        /// </summary>
+        /// <param name="htmlValue"></param>
+        /// <returns></returns>
+        public static string  GetNextUrl(string htmlValue)
+        {
+            List<BookContent> bookContents = new List<BookContent>();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlValue);
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//div[@id='list']/dl/dd/a");
+            int num = 2;
+            foreach(HtmlNode node in nodes)
+            {
+                if (num > 10)
+                {
+                    bookContents.Add(new BookContent { 
+                        chapter = node.InnerText ,
+                        Content= node.Attributes["href"].Value
+                    }) ;
+                }
+                num++;
+            }
+            bookContents[1].Content
+            return "";
+        }
+
+
+        /// <summary>
+        /// 返回响应的内容(解压后的压)
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static string GetUzipResponse(HttpWebRequest request)
+        {
+            string result = string.Empty;
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.ContentEncoding.ToLower().Contains("gzip"))//解压
+                {
+                    using (GZipStream stream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress))
+                    {
+                        using (StreamReader reader = new StreamReader(stream, Encoding.GetEncoding("GBK")))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+                else if (response.ContentEncoding.ToLower().Contains("deflate"))//解压
+                {
+                    using (DeflateStream stream = new DeflateStream(response.GetResponseStream(), CompressionMode.Decompress))
+                    {
+                        using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+                else
+                {
+                    using (Stream stream = response.GetResponseStream())//原始
+                    {
+                        using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// 通过网页chaset获取编码
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        static string GetHtml(string url, Encoding encoding)
+        {
+            byte[] buf = new WebClient().DownloadData(url);
+            if (encoding != null) return encoding.GetString(buf);
+            string html = Encoding.UTF8.GetString(buf);
+            encoding = GetEncoding(html);
+            if (encoding == null || encoding == Encoding.UTF8) return html;
+            return encoding.GetString(buf);
+        }
+        // 根据网页的HTML内容提取网页的Encoding
+        static Encoding GetEncoding(string html)
+        {
+            string pattern = @"(?i)\bcharset=(?<charset>[-a-zA-Z_0-9]+)";
+            string charset = Regex.Match(html, pattern).Groups["charset"].Value;
+            try { return Encoding.GetEncoding(charset); }
+            catch (ArgumentException) { return null; }
+        }
+    }
+}
---
+++ PushToKindle/PushToKindle/RequestOptions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushToKindle
+{
+    class RequestOptions
+    {
+        /// <summary>
+        /// 请求方式，GET或POST
+        /// </summary>
+        public string Method { get; set; }
+        /// <summary>
+        /// URL
+        /// </summary>
+        public Uri Uri { get; set; }
+        /// <summary>
+        /// 上一级历史记录链接
+        /// </summary>
+        public string Referer { get; set; }
+        /// <summary>
+        /// 超时时间（毫秒）
+        /// </summary>
+        public int Timeout = 15000;
+        /// <summary>
+        /// 启用长连接
+        /// </summary>
+        public bool KeepAlive = true;
+        /// <summary>
+        /// 禁止自动跳转
+        /// </summary>
+        public bool AllowAutoRedirect = false;
+        /// <summary>
+        /// 定义最大连接数
+        /// </summary>
+        public int ConnectionLimit = int.MaxValue;
+        /// <summary>
+        /// 请求次数
+        /// </summary>
+        public int RequestNum = 3;
+        /// <summary>
+        /// 可通过文件上传提交的文件类型
+        /// </summary>
+        public string Accept = "*/*";
+        /// <summary>
+        /// 内容类型
+        /// </summary>
+        public string ContentType = "application/x-www-form-urlencoded";
+        /// <summary>
+        /// 实例化头部信息
+        /// </summary>
+        private WebHeaderCollection header = new WebHeaderCollection();
+        /// <summary>
+        /// 头部信息
+        /// </summary>
+        public WebHeaderCollection WebHeader
+        {
+            get { return header; }
+            set { header = value; }
+        }
+        /// <summary>
+        /// 定义请求Cookie字符串
+        /// </summary>
+        public string RequestCookies { get; set; }
+        /// <summary>
+        /// 异步参数数据
+        /// </summary>
+        public string XHRParams { get; set; }
+    }
+}
